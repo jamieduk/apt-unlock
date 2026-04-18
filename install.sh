8493c36ad68d62f9afc6a528d879dacc2ddc12e9
Jay <jamied_uk@hotmail.com>
2026-04-18 09:28:06 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# (c) J~Net 2026
+# jnetai.com
+#
+#
+# ./install.sh
+#
+sudo cp apt-unlock /usr/bin/apt-unlock && sudo chmod +x /usr/bin/apt-unlock
+#
+echo "Now you can run this by typing apt-unlock!"
